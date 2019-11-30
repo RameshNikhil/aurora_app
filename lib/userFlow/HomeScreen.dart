6f9298e72a23624aba9d8fb4b15b9dcc5bd4edca
DeeplyDiligent <deep.bhattacharyya@monash.edu>
2019-11-30 21:47:45 +1100

    message stuff kinda done

--- lib/userFlow/HomeScreen.dart
+++ lib/userFlow/HomeScreen.dart
@@ -1,4 +1,5 @@
 import 'package:aurora_app/userFlow/CentralPage.dart';
+import 'package:aurora_app/userFlow/messagePage.dart';
 import 'package:flutter/material.dart';
 
 
@@ -21,7 +22,7 @@ class HomeScreen extends StatelessWidget {
       pageSnapping: true,
       physics: BouncingScrollPhysics(),
       children: <Widget>[
-
+        MessagePage(),
         CentralPage(locStore: locStore,)
 
         //Single Pages
